496533d7ab7d9bac69cd4774254cdd30bb64d91d
yts798 <yts798@163.com>
2023-03-22 10:41:53 +0800

    start database

---
+++ auto_update/crawler_update_3.py
@@ -0,0 +1,77 @@
+import requests
+import json
+import pandas as pd
+import datetime
+
+login_header = {
+    "origin": "https://www.jisilu.cn",
+    "Referer": "https://www.jisilu.cn/account/login/",
+    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.44"
+}
+login_url = 'https://www.jisilu.cn/webapi/account/login_process/'
+login_info = {
+   "return_url": "https://www.jisilu.cn/",
+   "user_name": "c3bccbdd0540a4d14f828a38e013e505",
+   "password": "d6fc78015d1d08ac20375ffa2b3d7361",
+   "aes": 1,
+   "auto_login": 0
+}
+
+
+
+session = requests.session()
+
+session.post(login_url, data = login_info, headers = login_header)
+data_header = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.44'
+}
+
+# data_header = {
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36',
+#     'Cookie': 'kbz_newcookie=1; kbzw__Session=rr4iblgsj1muvcj8ldse5v13p7; Hm_lvt_164fe01b1433a19b507595a43bf58262=1679282750,1679388637,1679391827,1679392007; Hm_lpvt_164fe01b1433a19b507595a43bf58262=1679392007; kbzw__user_login=7Obd08_P1ebax9aX2M7s2vDwlLOO4MLn7OzY69DVv6GUrausrpSpw6Wqpc2t0qeYqpqwrNfeltWRqa-lna2j2oOxjuzg19a-m5Ktrq6gqZ2YnaOiy9XQo5KmmK2srpupn6iDsY7MuNHVjL3Q7uLh1dqbrJCmgZ_O3ObF39jnmcO9mZ2nkKacl87c5peknJTxq52ijLjS5s3cztjarNnVo66ooKefrYKerL_LwMSNkM3d5NqJwNHazeWKl7rb6tDdxqOqppqnnKWSpJGXytTewuLKo66ooKefrQ..'
+# }
+
+
+
+# jsl data
+data_url = "https://www.jisilu.cn/data/cbnew/cb_list_new/?___jsl=LST___t=1637410410639"
+
+
+
+# retrieve data
+# data_response =  session.get(data_url, headers = try_header)
+data_response = session.get(data_url, headers=data_header)
+
+
+data = data_response.json()
+
+td = datetime.datetime.today().date()
+clean_data = []
+for i in range(0,len(data['rows'])):
+    if data['rows'][i]["cell"]['bond_id'] != '132018':
+        if data['rows'][i]["cell"]['list_dt'] != None:
+            ipo_dt = datetime.datetime.strptime(data['rows'][i]["cell"]['list_dt'], '%Y-%m-%d').date()
+            if ipo_dt <= td:
+                clean_data.append(data['rows'][i]["cell"])
+                
+                
+     
+    
+
+            
+full = list(clean_data[0].keys())
+light = ['bond_id', 'bond_nm', 'list_dt', 'price','premium_rt','ytm_rt','curr_iss_amt','turnover_rt','year_left','stock_id','stock_nm', 'market_cd']
+t = td.strftime('%Y-%m-%d')
+name_full = t + '-full.csv'
+name_light = t + '-light.csv'
+
+df_full = pd.DataFrame(clean_data)[full]
+df_light = pd.DataFrame(clean_data)[light]
+print(len(data['rows']))
+print(len(clean_data))
+df_full.to_csv(name_full,index=None, encoding='utf_8_sig')
+df_light.to_csv(name_light,index=None, encoding='utf_8_sig')
+
+
+
+
---
+++ auto_update/to_database.py
@@ -0,0 +1,145 @@
+import numpy as np
+import pickle
+import time 
+import os
+import requests
+import json
+import pandas as pd
+import datetime
+from datetime import datetime
+from operator import itemgetter
+
+
+data_folder_path = 'C:\\Users\\goodluck\\Desktop\\database_workspace'
+raw_data_path =  "C:\\Users\\goodluck\\Desktop\\database_workspace\\raw_data"
+storage_path =  "C:\\Users\\goodluck\\Desktop\\database_workspace\\database_storage"
+update_path =   "C:\\Users\\goodluck\\Desktop\\database_workspace\\auto_update"
+DTBS_path = os.path.join(storage_path, "DTBS.pkl")
+
+
+def find_diff(list1,list2):
+    bt = set(list1).intersection(set(list2)) 
+    al = set(list1).union(set(list2))
+    new = set(list1).difference(set(list2)) 
+    die = set(list2).difference(set(list1))
+#     print("union:" +str(len(A)))
+#     print("intersect:" +str(len(B)))
+#     print("only in list 1 " +str(len(C)))
+#     print("only in list 2 " +str(len(D)))
+    return bt ,al, new, die
+
+def convert_code(ori_code, mkt):
+    m = '.' + mkt[0:2].upper()
+    ori_code = str(ori_code)
+    l = len(ori_code)
+    while l < 6:
+        ori_code = '0' + ori_code
+        l += 1
+    return ori_code + m
+    
+
+# re-open
+with open(DTBS_path, 'rb') as f:  
+    DTBS = pickle.load(f)
+    
+    
+tdd =datetime.today()
+td = tdd.strftime('%Y-%m-%d')
+name = td + '-light.csv'
+
+
+
+new_df = pd.read_csv(name, encoding='utf-8')
+new_df.set_index('bond_id', inplace=True)
+
+new_code = [str(i) for i in list(new_df.index)]
+new_key = []
+for i in new_code:
+    one = i + '.' + new_df.loc[int(i)]['market_cd'][0:2].upper()
+    new_key.append(one)
+old_key = [i for i in list(DTBS['B'].keys())]
+
+
+bt, al, new, die = find_diff(new_key,old_key)
+
+for code in bt:
+
+    DTBS['A'][code][td] = dict()
+    DTBS['A'][code][td]['cpr'] = new_df.loc[int(code[0:6])]['premium_rt']
+    DTBS['A'][code][td]['dp'] = new_df.loc[int(code[0:6])]['price']
+    DTBS['A'][code][td]['dl'] = new_df.loc[int(code[0:6])]['premium_rt'] + new_df.loc[int(code[0:6])]['price']
+    DTBS['A'][code][td]['ytm'] = new_df.loc[int(code[0:6])]['ytm_rt']
+    DTBS['A'][code][td]['bl'] = new_df.loc[int(code[0:6])]['curr_iss_amt']
+    DTBS['A'][code][td]['trt'] = new_df.loc[int(code[0:6])]['turnover_rt']
+    DTBS['A'][code][td]['yl'] = new_df.loc[int(code[0:6])]['year_left']
+    
+for code in new:
+    DTBS['A'][code] = dict()
+    for dt in DTBS['D']['day']:
+        DTBS['A'][code][dt] = dict()
+        DTBS['A'][code][dt]['cpr'] = np.nan
+        DTBS['A'][code][dt]['dp'] = np.nan
+        DTBS['A'][code][dt]['dl'] = 999        
+        DTBS['A'][code][dt]['ytm'] = np.nan
+        DTBS['A'][code][dt]['bl'] = np.nan
+        DTBS['A'][code][dt]['trt'] = np.nan
+        DTBS['A'][code][dt]['yl'] = np.nan
+    
+   
+    DTBS['A'][code][td] = dict()
+    DTBS['A'][code][td]['cpr'] = new_df.loc[int(code[0:6])]['premium_rt']
+    DTBS['A'][code][td]['dp'] = new_df.loc[int(code[0:6])]['price']
+    DTBS['A'][code][td]['dl'] = new_df.loc[int(code[0:6])]['premium_rt'] + new_df.loc[int(code[0:6])]['price']
+    DTBS['A'][code][td]['ytm'] = new_df.loc[int(code[0:6])]['ytm_rt']
+    DTBS['A'][code][td]['bl'] = new_df.loc[int(code[0:6])]['curr_iss_amt']
+    DTBS['A'][code][td]['trt'] = new_df.loc[int(code[0:6])]['turnover_rt']
+    DTBS['A'][code][td]['yl'] = new_df.loc[int(code[0:6])]['year_left']
+    
+    DTBS['B'][code] = dict()
+    DTBS['B'][code]['ipo_dt'] = new_df.loc[int(code[0:6])]['list_dt']
+    DTBS['B'][code]['cn'] = new_df.loc[int(code[0:6])]['bond_nm']
+    DTBS['B'][code]['sc'] = convert_code(new_df.loc[int(code[0:6])]['stock_id'], new_df.loc[int(code[0:6])]['market_cd'])
+    DTBS['B'][code]['sn'] = new_df.loc[int(code[0:6])]['stock_nm']
+    DTBS['B'][code]['cat1'] = np.nan
+    DTBS['B'][code]['cat2'] = np.nan                 
+    
+    
+for code in die:
+    DTBS['A'][code][td] = dict()
+    DTBS['A'][code][td]['cpr'] = np.nan
+    DTBS['A'][code][td]['dp'] = np.nan
+    DTBS['A'][code][td]['dl'] = 999
+    DTBS['A'][code][td]['ytm'] = np.nan
+    DTBS['A'][code][td]['bl'] = np.nan
+    DTBS['A'][code][td]['trt'] = np.nan
+    DTBS['A'][code][td]['yl'] = 0
+    
+
+DTBS['D']['day'].append(td)
+if tdd.weekday() == 0:
+    DTBS['D']['mon'].append(td)
+elif tdd.weekday() == 1:
+    DTBS['D']['tue'].append(td)
+if tdd.weekday() == 2:
+    DTBS['D']['wed'].append(td)
+if tdd.weekday() == 3:
+    DTBS['D']['thu'].append(td)
+if tdd.weekday() == 4:
+    DTBS['D']['fri'].append(td)
+    
+index = requests.get('https://www.jisilu.cn/data/idx_performance/list/flex_idx')
+index_dict = index.json()
+prc = ''
+for term in index_dict['rows']:
+    if term['id'] == '000832':
+        prc = term['cell']['price']
+        break
+DTBS['C']['zi'][td] = prc
+    
+    
+# save
+    
+f_save = open(DTBS_path, 'wb')
+pickle.dump(DTBS, f_save)
+f_save.close()
+
